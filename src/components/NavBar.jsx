8f307c960d30d6e65a8bc7f249aa2c0f37a1c4b0
supreeta-anand-byatnal <supreeta.byatnal@gmail.com>
2023-05-20 21:45:46 -0400

    Initial commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
+      <div>
+        <h1 className="text-5xl font-signature ml-2">Yash</h1>
+      </div>
+      </div>
+  );
+};
+
+export default NavBar
