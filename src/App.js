4d0fade75702e1dc0aaeaa35e5f9076d3d4248e5
supreeta-anand-byatnal <supreeta.byatnal@gmail.com>
2023-05-22 16:50:54 -0400

    Links and home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import NavBar from './components/NavBar';
-
+import Home from './components/Home';
+import Links from './components/Links';
 function App() {
   return (
     <div>
       <NavBar/>
+      <Home/>
+      <Links/>
+
     </div>
   );
 }
