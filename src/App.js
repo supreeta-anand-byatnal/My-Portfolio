36676a34bfa2bb42594a9cae04f6e7669273795b
supreeta-anand-byatnal <supreeta.byatnal@gmail.com>
2023-05-22 18:53:38 -0400

    Contact.jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import NavBar from './components/NavBar';
 import Home from './components/Home';
 import Links from './components/Links';
+import Contact from './components/Contact';
+
 function App() {
   return (
     <div>
       <NavBar/>
       <Home/>
       <Links/>
+      <Contact/>
+      
 
     </div>
   );
