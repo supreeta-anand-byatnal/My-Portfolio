f7ee71e10866ebb54b5e695e6ea811ac27673d05
supreeta-anand-byatnal <supreeta.byatnal@gmail.com>
2023-05-28 03:03:57 -0400

    Experience

---
+++ src/components/timelineElements.js
@@ -0,0 +1,50 @@
+let timelineElements = [
+  {
+    id: 1,
+    title: "Software Engineering Intern",
+    location: "Sovos LLC, MA, US",
+    date: "June 2023 - present",
+    description   : "Incoming intern",
+    icon: "work",
+  },
+  {
+    id: 2,
+    title: "New York University, Courant Institute of Mathematical Sciences",
+    location: "New York City, New York",
+    description:"Master of Science in Computer Science",
+    date: " Expected : May 2024",
+    icon: "school",
+  },
+  {
+    id: 3,
+    title: "Software Development Engineer",
+    location: "McAfee Inc, India",
+    description:"Reduced the time needed for policy violation detection to 50% by developing Continuous Evaluation feature in the java based CASB application called MVision Cloud.",
+    date: "August 2020 - June 2022",
+
+    icon: "work",
+  },
+  {
+    id: 4,
+    title: "Undergraduate Technical Intern",
+    location: "McAfee Inc, India",
+    description:
+      "Designed and automated over 1000+ test cases for the existing features in the project using Pytest library which boosted the efficiency of regression by 48%.", 
+    date: "February 2020 - August 2020",
+    icon: "work",
+  },
+ 
+  {
+    id: 5,
+    title: "BMS College of Engineering",
+    location: "Bangalore, India",
+    description:
+      "Bachelor of Engineering in Information Science and Engineering",
+       buttonText: "College Projects",
+    date: "2016 - 2020",
+    icon: "school",
+  },
+  
+];
+
+export default timelineElements;
