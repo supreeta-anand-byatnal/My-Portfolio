c03b638bc946679cbee8dc528878ba17b29d31b6
supreeta-anand-byatnal <supreeta.byatnal@gmail.com>
2023-05-29 16:26:05 -0400

    Tabs in experience page

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,62 +1,92 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ReactComponent as WorkIcon } from "./work.svg";
 import { ReactComponent as SchoolIcon } from "./school.svg";
 import timelineElements from "./timelineElements";
-import {
-  VerticalTimeline,
-  VerticalTimelineElement,
-} from "react-vertical-timeline-component";
+import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 
 const Experience = () => {
+  const [activeTab, setActiveTab] = useState("experience");
+
+  const handleTabChange = (tabName) => {
+    setActiveTab(tabName);
+  };
+
   let workIconStyles = { background: "#4d7c0f" };
   let schoolIconStyles = { background: "#4d7c0f" };
 
   return (
-    <div name="experience" className="h-100 w-full bg-gradient-to-b from-black via-black to-gray-800">
-      <div className="w-full max-w-screen-lg mx-auto">
-        <div className="text-center">
-          <h1 className="text-5xl mt-20 mb-15 font-bold border-b-4 text-white border-lime-700 inline-block">
-            Experience
-          </h1>
-        </div>    
-        </ div> 
-      <div className='py-5'><VerticalTimeline>
-        {timelineElements.map((element) => {
-          let isWorkIcon = element.icon === "work";
-          let showButton =
-            element.buttonText !== undefined &&
-            element.buttonText !== null &&
-            element.buttonText !== "";
+    <div>
+      <div className="tabs">
+        <div className={`tab ${activeTab === "experience" ? "active" : ""}`} onClick={() => handleTabChange("experience")}>Experience</div>
+        <div className={`tab ${activeTab === "schoolActivities" ? "active" : ""}`} onClick={() => handleTabChange("schoolActivities")}>School Activities</div>
+        <div className={`tab ${activeTab === "skills" ? "active" : ""}`} onClick={() => handleTabChange("skills")}>Skills</div>
+      </div>
 
-          return (
-            
-            <VerticalTimelineElement
-            
-              key={element.id}
-              date={element.date}
-              dateClassName="date"
-              iconStyle={isWorkIcon ? workIconStyles : schoolIconStyles}
-              icon={isWorkIcon ? <WorkIcon /> : <SchoolIcon />}
-            >
-              <div className="bg-lime-700 p-2 rounded-md text-white">
-
-              <h3 className="first-letter:vertical-timeline-element-title  text-lg">
-                {element.title}
-              </h3>
-              <h5 className="vertical-timeline-element-subtitle text-sm">
-                {element.location}
-              </h5>
+      {activeTab === "experience" && (
+        <div className="tab-content">
+          <div name="experience" className="h-full w-full bg-gradient-to-b from-black via-black to-gray-800">
+            <div className="w-2/3 mx-auto h-full">
+              <div className="text-center">
+                <h1 className="text-5xl mt-20 mb-15 font-bold border-b-4 text-white border-lime-700 inline-block">Experience</h1>
               </div>
-              <p id="description">{element.description}</p>
-          
-            </VerticalTimelineElement>
-          );
-        })}
-      </VerticalTimeline>
+              <div className='h-full py-5'>
+                <VerticalTimeline>
+                  {timelineElements.map((element) => {
+                    let isWorkIcon = element.icon === "work";
+                    let showButton = element.buttonText !== undefined && element.buttonText !== null && element.buttonText !== "";
+
+                    return (
+                      <VerticalTimelineElement
+                        key={element.id}
+                        date={element.date}
+                        dateClassName="date"
+                        iconStyle={isWorkIcon ? workIconStyles : schoolIconStyles}
+                        icon={isWorkIcon ? <WorkIcon /> : <SchoolIcon />}
+                      >
+                        <div className="bg-lime-700 p-2 rounded-md text-white">
+                          <h3 className="first-letter:vertical-timeline-element-title text-md">{element.title}</h3>
+                          <i><h6 className="vertical-timeline-element-subtitle text-sm">{element.location}</h6></i>
+                        </div>
+                      </VerticalTimelineElement>
+                    );
+                  })}
+                </VerticalTimeline>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {activeTab === "schoolActivities" && (
+        <div className="tab-content">
+          <div name="school-activities" className="h-full w-full bg-gradient-to-b from-black via-black to-gray-800">
+            <div className="w-2/3 mx-auto h-full">
+              <div className="text-center">
+                <h1 className="text-5xl mt-20 mb-15 font-bold border-b-4 text-white border-lime-700 inline-block">School Activities</h1>
+              </div>
+              {/* Content for the School Activities tab */}
+            </div>
+          </div>
+        </div>
+      )}
+
+...
+
+{activeTab === "skills" && (
+  <div className="tab-content">
+    <div name="skills" className="h-full w-full bg-gradient-to-b from-black via-black to-gray-800">
+      <div className="w-2/3 mx-auto h-full">
+        <div className="text-center">
+          <h1 className="text-5xl mt-20 mb-15 font-bold border-b-4 text-white border-lime-700 inline-block">Skills</h1>
+        </div>
+        {/* Content for the Skills tab */}
       </div>
     </div>
-  );
+  </div>
+)}
+</div>
+);
 };
 
 export default Experience;
